4d9be2938d13035af76949a9e8b7d071ba8620bc
i_82 <i.82@qq.com>
2017-05-14 00:22:46 +0200

    Create Questionnaire (Quiz) Page

--- src/main/java/pandaroux/Controller/DefaultController.java
+++ src/main/java/pandaroux/Controller/DefaultController.java
@@ -37,6 +37,11 @@ public class DefaultController {
         return "teacher/question";
     }
 
+    @RequestMapping(value = "/teacher/questionnaire")
+    public String teacherQuestionnaire() {
+        return "teacher/questionnaire";
+    }
+
     @RequestMapping(value = "/student")
     public String student() {
         return "redirect:/student/index";
@@ -51,6 +56,5 @@ public class DefaultController {
     public String studentCourse() {
         return "student/course";
     }
-
 
 }
