3e5fa268acaa831920d1b3086c38e45dea8dc683
Vincenthays <vincenthays95@gmail.com>
2017-05-15 13:59:17 +0200

    api

--- src/main/java/pandaroux/Service/Entity/LectureService.java
+++ src/main/java/pandaroux/Service/Entity/LectureService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import pandaroux.Entity.Lecture;
 import pandaroux.Repository.LectureRepository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +26,41 @@ public class LectureService {
         return data;
     }
 
-    public void deleteById(int id) {
+    public Map deleteById(int id) {
         lectureRepository.delete(id);
+
+        Map data = new HashMap();
+        data.put("result", "succeed");
+
+        return data;
+    }
+
+    public Map modify(int id_lecture, Map lecture) throws ParseException {
+
+        Lecture lectureDB = lectureRepository.findOne(id_lecture);
+
+        if (lecture.containsKey("name")) {
+            lectureDB.setName((String) lecture.get("name"));
+        }
+
+        if (lecture.containsKey("date")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+            lectureDB.setDate(sdf.parse((String) lecture.get("date")));
+        }
+
+        if (lecture.containsKey("commentary")) {
+            lectureDB.setCommentary((String) lecture.get("commentary"));
+        }
+
+        if (lecture.containsKey("rate")) {
+            lectureDB.setRate((Integer) lecture.get("rate"));
+        }
+
+        lectureRepository.save(lectureDB);
+
+        Map data = new HashMap();
+        data.put("result", "succeed");
+
+        return data;
     }
 }
