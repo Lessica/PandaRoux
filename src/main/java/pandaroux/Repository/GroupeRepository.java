9c6551f7917058877f650491216ba9f7f5acab77
i_82 <i.82@qq.com>
2017-06-13 17:24:10 +0200

    Student Group Page.

--- src/main/java/pandaroux/Repository/GroupeRepository.java
+++ src/main/java/pandaroux/Repository/GroupeRepository.java
@@ -16,9 +16,13 @@ public interface GroupeRepository extends CrudRepository<Groupe, Integer> {
     @Query("SELECT g.id AS id, g.name AS name FROM Groupe g WHERE g.teacher.id = ?1")
     List<Map> getGroupes(int teacherId);
 
-    @Query("SELECT s.id AS id, s.name AS name, s.first_name AS first_name FROM Groupe g JOIN g.students s  WHERE g.id = ?1")
+    @Query("SELECT s.id AS id, s.name AS name, s.first_name AS first_name FROM Groupe g JOIN g.students s WHERE g.id = ?1")
     List<Map> getGroupStudents(int groupeId);
 
-    @Query("SELECT l.id AS id, l.name AS name, l.date AS date, l.rate AS rate, l.commentary AS commentary FROM Groupe g JOIN g.lectures l  WHERE g.id = ?1")
+    @Query("SELECT l.id AS id, l.name AS name, l.date AS date, l.rate AS rate, l.commentary AS commentary FROM Groupe g JOIN g.lectures l WHERE g.id = ?1")
     List<Map> getGroupLectures(int groupeId);
+
+    @Query("SELECT g.id AS id, g.name AS name, (SELECT COUNT(*) FROM g.lectures) AS lecture_count, (SELECT COUNT(*) FROM g.quizzes) AS quiz_count FROM Groupe g JOIN g.students s WHERE s.id = ?1")
+    List<Map> getStudentGroups(int studentId);
+
 }
